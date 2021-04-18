8b8946000c594bb2d99a617cb1c891f1cb6dbe20
MatiasBesson <matias_besson@hotmail.com>
2021-04-18 20:03:12 -0300

    Cargo consulta de prueba de datos

---
+++ Tests/01.query_de_prueba.sql
@@ -0,0 +1,14 @@
+INSERT INTO cliente (cuit, razon_social, domicilio) VALUES ('20345678521', 'MATEVIRTUAL SA', 'Paraguay 1234');
+INSERT INTO cliente (cuit, razon_social, domicilio) VALUES ('30332251323', 'CYBERLOTERIA SA', 'Defensa 838');
+
+
+INSERT INTO proyecto (nombre, id_cliente) VALUES ('TechArg', 1);
+INSERT INTO proyecto (nombre, id_cliente) VALUES ('IT_Pueyrredon', 2);
+INSERT INTO proyecto (nombre, id_cliente) VALUES ('UCESgroup', 1);
+
+
+INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Roberto', 'Gimenez', 37123147, 28);
+INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Romina', 'Perez', 35210481, 31);
+INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Carlos', 'Rodriguez', 42004874, 24);
+INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Lucia', 'Sosa', 30234657, 35);
+INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Julian', 'Romero', 33487569, 33);
