6279b014d4c575c7176fe8b45a99d8425b7f3781
MatiasBesson <matias_besson@hotmail.com>
2021-04-19 00:32:38 -0300

    Prueba cargar datos por procedure

--- Tests/01.query_de_prueba.sql
+++ Tests/01.query_de_prueba.sql
@@ -1,14 +1,28 @@
-INSERT INTO cliente (cuit, razon_social, domicilio) VALUES ('20345678521', 'MATEVIRTUAL SA', 'Paraguay 1234');
-INSERT INTO cliente (cuit, razon_social, domicilio) VALUES ('30332251323', 'CYBERLOTERIA SA', 'Defensa 838');
+CALL carga_nuevo_cliente ('20345678521', 'MATEVIRTUAL SA', 'Paraguay 1234');
+CALL carga_nuevo_cliente ('30332251323', 'CYBERLOTERIA SA', 'Defensa 838');
 
 
-INSERT INTO proyecto (nombre, id_cliente) VALUES ('TechArg', 1);
-INSERT INTO proyecto (nombre, id_cliente) VALUES ('IT_Pueyrredon', 2);
-INSERT INTO proyecto (nombre, id_cliente) VALUES ('UCESgroup', 1);
+CALL carga_nuevo_participante ('Roberto', 'Gimenez', 37123147, 28);
+CALL carga_nuevo_participante ('Romina', 'Perez', 35210481, 31);
+CALL carga_nuevo_participante ('Carlos', 'Rodriguez', 42004874, 24);
+CALL carga_nuevo_participante ('Lucia', 'Sosa', 30234657, 35);
+CALL carga_nuevo_participante ('Julian', 'Romero', 33487569, 33);
 
 
-INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Roberto', 'Gimenez', 37123147, 28);
-INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Romina', 'Perez', 35210481, 31);
-INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Carlos', 'Rodriguez', 42004874, 24);
-INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Lucia', 'Sosa', 30234657, 35);
-INSERT INTO participante (nombre, apellido, dni, edad) VALUES ('Julian', 'Romero', 33487569, 33);
+CALL carga_nuevo_proyecto ('TechArg', 1);
+CALL carga_nuevo_proyecto ('IT_Pueyrredon', 2);
+CALL carga_nuevo_proyecto ('UCESgroup', 1);
+
+
+CALL carga_participacion (1, 1, 'Project Manager', 100);
+CALL carga_participacion (3, 1, 'Desarrollador', 80);
+CALL carga_participacion (2, 1, 'Tester', 40);
+CALL carga_participacion (5, 2, 'Project Manager', 120);
+CALL carga_participacion (4, 2, 'Desarrollador', 100);
+CALL carga_participacion (2, 2, 'Administrador', 80);
+CALL carga_participacion (3, 2, 'Tester', 60);
+CALL carga_participacion (3, 3, 'Project Manager', 140);
+CALL carga_participacion (1, 3, 'Desarrollador', 160);
+CALL carga_participacion (2, 3, 'Administrador', 100);
+CALL carga_participacion (4, 3, 'DevOps', 120);
+CALL carga_participacion (5, 3, 'Tester', 100);
